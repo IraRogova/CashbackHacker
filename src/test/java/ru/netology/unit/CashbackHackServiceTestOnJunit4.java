7f488946d51f7fe916bd7c9327ca2c641a6bcb80
Irina Rogova <buyersbirds@gmail.com>
2021-01-20 17:54:11 +0600

    added testOnJunit4

--- src/test/java/ru/netology/unit/CashbackHackServiceTestOnJunit4.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTestOnJunit4.java
@@ -2,10 +2,10 @@ package ru.netology.unit;
 
 import org.junit.Test;
 
-//import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-public class CashbackHackServiceTest {
+public class CashbackHackServiceTestOnJunit4 {
+
     @Test
     public void shouldRemainBoundaryIfLess1000(){
         CashbackHackService  cashbackHack = new CashbackHackService();
